42620fcd7399f686ce67cd439bbb386b127a0533
Rangga <dwi.rangga@infoflow.co.id>
2020-02-19 13:16:38 +0700

    prepare for heroku

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ require('./models/Train');
 const init = async () => {
 
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+    port: process.env.PORT || 3000,
+    host: process.env.YOUR_HOST || '0.0.0.0'
   });
 
   server.route({
@@ -34,12 +34,6 @@ const init = async () => {
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
-
-process.on('unhandledRejection', (err) => {
-
-  console.log(err);
-  process.exit(1);
-});
 
 process.on('unhandledRejection', (err) => {
 
