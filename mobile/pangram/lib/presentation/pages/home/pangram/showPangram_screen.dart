54822c148e6ee67aa0aa95cefaa8cc3d512bcdb6
Rangga <dwi.rangga@infoflow.co.id>
2020-02-24 16:56:10 +0700

    deploy post anagram to heroku

--- mobile/pangram/lib/presentation/pages/home/pangram/showPangram_screen.dart
+++ mobile/pangram/lib/presentation/pages/home/pangram/showPangram_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PangramScreen extends StatefulWidget {
   PangramScreen({Key key}) : super(key: key);
@@ -8,10 +10,52 @@ class PangramScreen extends StatefulWidget {
 }
 
 class _PangramScreen extends State<PangramScreen> {
-  String _textParagraph;
+  String _textParagraph = '';
+  String _value;
+  String _TextField1;
+  String _TextField2;
+
+  void fetchPangram() async {
+    Map<String, String> header = {'Content-Type': 'application/json'};
+    var response = await http.get('http://pangram-of-the-day.herokuapp.com/pangram', headers: header);
+    if (response.statusCode == 200) {
+      var _json = json.decode(response.body);
+      var data = _json['data'];
+      setState(() {
+        _textParagraph = data['text'];
+      });
+    }
+  }
+
+  void getAnagram() async{
+    if(_TextField1.length > 0 && _TextField2.length > 0){
+      Map<String, dynamic> map = new Map<String, dynamic>();
+      map["anagram"] = _TextField1.toString();
+      map["anagram1"] = _TextField1.toString();
+
+      var response = await http.post(
+        'http://pangram-of-the-day.herokuapp.com/', 
+        headers: {'Content-Type': 'application/json'}, 
+        body: json.encode(map)
+      );
+
+      print(' anagram => ' + response.statusCode.toString());
+    }
+  }
+
+   void initPage(){
+      if(_value== null){
+        setState(() {
+          _value = 'Pangram';
+        });
+      } 
+   }
+
   void initState() {
     super.initState();
-    _textParagraph = 'hello world';
+    fetchPangram();
+    initPage();
+    print('_value => ' + _value.toString());
   }
 
   Widget build(context) {
@@ -31,26 +75,103 @@ class _PangramScreen extends State<PangramScreen> {
           child: Column(
             children: <Widget>[
               Container(
-                alignment: new FractionalOffset(0.0, 0.0),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.refresh,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {},
-                ),
+                alignment: Alignment.center,
+                child: new DropdownButton<String>(
+                  items: <String>['Pangram', 'Angram'].map((String value) {
+                    return new DropdownMenuItem<String>(
+                      value: value,
+                      child: new Text(value),
+                    );
+                  }).toList(),
+                  value: _value,
+                  isExpanded: true,
+                  onChanged: (val) {
+                    setState(() {
+                      _value = val;
+                    });
+                  },
+                )
               ),
-              Container(margin: EdgeInsets.only(bottom: 10.0)),
-              Container(
-                child: Text(
-                  _textParagraph,
-                  textAlign: TextAlign.left,
-                ),
-              ),
+              _value.toString() == 'Pangram' ? _buildPangram() : _buildAngram()
             ],
           ),
         ),
       ),
     );
   }
+
+  Widget _buildPangram() {
+    return new Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                fetchPangram();
+              },
+            ),
+          ),
+          Container(
+            child: Text(
+              _textParagraph,
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ]
+      )
+    );
+  }
+
+   Widget _buildAngram() {
+     return new Container(
+      child: Column(
+        children: <Widget>[
+          Container(margin: EdgeInsets.only(bottom: 10.0)),
+          Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            child: new TextField(
+                decoration: InputDecoration(
+                hintText: "Anagram 1...",
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _TextField1 = value;
+                });
+              },
+            )
+          ),
+          Container(margin: EdgeInsets.only(bottom: 30.0)),
+          Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            child: new TextField(
+                decoration: InputDecoration(
+                hintText: "Anagram 2...",
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _TextField2 = value;
+                });
+              },
+            )
+          ),
+          Container(margin: EdgeInsets.only(bottom: 10.0)),
+          Container(
+            child: RaisedButton(
+              child: Text('Check!'),
+              onPressed: (){
+                getAnagram();
+              }
+            )
+          )
+        ]
+      )
+    );
+   }
 }
